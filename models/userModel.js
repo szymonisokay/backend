d88cd8a85501ec6757f276179bcd32988e7a90a0
Szymon <s.walach@soniqsoft.pl>
2022-05-30 12:12:21 +0200

    user model changed

---
+++ models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+  is_admin: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+module.exports = mongoose.model('User', UserSchema)
